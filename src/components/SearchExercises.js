6ad12ea76a759df47996530e5f5114a8561fce28
unknown <mdtanbir576@gmail.com>
2022-06-21 17:58:24 +0600

    serach-component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import {Box,Stack,TextField,Typography} from '@mui/material'
+
 
 const SearchExercises = () => {
+
+
     return (
-        <div>
+        <Stack alignItems="center" mt='37px' justifyContent="center" p="20px">
+            <Typography fontWeight="600" fontSize='44px' xs={{fontSize:{lg:'44px',xs:'30px'}}} mb="50px" textAlign="center">
+                Awsome Exercises You<br/> Know
+            </Typography>
             
-        </div>
+        </Stack>
     );
 };
 
