8c71a6804e5c39094f83061ecad98e3feb9d59a6
unknown <mdtanbir576@gmail.com>
2022-06-21 18:19:22 +0600

    Search component design

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,5 +1,6 @@
 import React,{useEffect,useState} from 'react';
-import {Box,Stack,TextField,Typography} from '@mui/material'
+import {Box,Stack,TextField,Typography,Button} from '@mui/material'
+import { textTransform } from '@mui/system';
 
 
 const SearchExercises = () => {
@@ -10,6 +11,33 @@ const SearchExercises = () => {
             <Typography fontWeight="600" fontSize='44px' xs={{fontSize:{lg:'44px',xs:'30px'}}} mb="50px" textAlign="center">
                 Awsome Exercises You<br/> Know
             </Typography>
+            <Box position="relative" mb="72px">
+                <TextField sx={{
+                    input:{fontWeight:'700',
+                   border:'none',
+                   borderRadius:'4px'
+                },
+                width:{lg:'800px', xs:'350px'},
+                backgroundColor:'#fff',
+                borderRadius:'40px'
+
+                }} height="76px"
+                 value="" onChange={(e) => {}}
+                 placeholder="Search Exercises"
+                />
+                <Button className='search-btn' sx={{bgcolor:'#FF2625',
+                 color:'#fff',
+                 textTransform:'none',
+                 width:{lg:'175px', xs:'80px'},
+                 fontSize:{lg:'20px',xs:'14px'},
+                 height:'56px',
+                 position:'absolute'
+            }}>Search</Button>
+
+               
+                
+
+            </Box>
             
         </Stack>
     );
